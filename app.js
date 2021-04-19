f7bcd87b3b11881c39ae340e117e08ef982593f4
s541910 <77760915+s541910@users.noreply.github.com>
2021-04-19 18:49:01 -0500

    added files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+;
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://ramu:ramu1234@cluster0.red75.mongodb.net/sauces?retryWrites=true&w=majority',
@@ -14,10 +14,25 @@ var slotRouter = require('./routes/slot')
 var starsRouter = require('./routes/stars')
 var sauces = require("./models/sauces");
 var resourceRouter=require('./routes/resource')
-// We can seed the collection if needed on server start
-
-
 var app = express();
+// We can seed the collection if needed on server start
+var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +42,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+var Account =require('./models/account'));
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
